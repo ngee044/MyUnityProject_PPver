a70f270f4e85f9198890ed78afb9734ad3adeda3
mgee044 <mgee044@naver.com>
2018-10-30 21:47:15 +0900

    Commit Add CShap Script...

---
+++ Assets/dotSc/Move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    int m_nID;
+
+    public int ID
+    {
+        set { m_nID = value; }
+        get { return m_nID; }
+    }
+
+    private void Awake()
+    {
+        //Do First function
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
