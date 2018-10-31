290a4dbab85ce566a260f447df4f16b1d48e1d78
mgee044 <mgee044@naver.com>
2018-10-31 21:48:42 +0900

    MoveEx SC Add and Practice (Move_ ...

---
+++ Assets/dotSc/MoveEx.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    int m_nID;
+    public int InputCnt = 0;
+    public int Speed = 30;
+
+    public int ID
+    {
+        set { m_nID = value; }
+        get { return m_nID; }
+    }
+
+    private void Awake()
+    {
+        //Do First function
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+            InputCnt++;
+            Debug.Log("input Key W Down");
+
+            if (InputCnt >= 5)
+            {
+                InputCnt = 0;
+                transform.gameObject.SetActive(false);
+                Debug.Break();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+}
