13cc93303ab11307f47e9eb8bb69a01efe106e8f
LeeHyunKyu <mgee044@naver.com>
2018-11-09 21:51:30 +0900

    collison Class Add

---
+++ Assets/Script/World/Collison.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collison : MonoBehaviour {
+
+	private void OnCollisionEnter(Collision rect)
+	{
+		Debug.Log("Collision");
+	}
+
+	private void OnCollisionStay(Collision rect)
+	{
+		Debug.Log("Collision + ing~");
+	}
+
+	private void OnCollisionExit(Collision rect)
+	{
+		Debug.Log("Collision Exit");
+	}
+}
