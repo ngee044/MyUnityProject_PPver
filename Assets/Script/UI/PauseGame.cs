d5f0450535b656b8e21674a7dd264a3f9c777d40
mgee044 <mgee044@naver.com>
2018-11-13 21:52:17 +0900

    Skill UI Add and Image Study

--- Assets/Script/UI/PauseGame.cs
+++ Assets/Script/UI/PauseGame.cs
@@ -34,19 +34,20 @@ public class PauseGame : MonoBehaviour {
         string title = "게임 재시작";
         string text = "정말 게임을 다시 시작 하시겠습니까?";
 
-        MessageBox.Show(title, text, new MsgOptions
-        {
-                            cancelButtonTitle = "NO",
-            CancelButtonDelegate = () =>
-                            {
-                                  Debug.Log("NO");
-                            },
-                            okButtonTitle = "Yes",
-            OkButtonDelegate = () =>
-                           {
-                             Debug.Log("Yes");
-                           }
-                        });
+        MessageBox.Show(title, text, 
+            new MsgOptions
+            {
+                cancelButtonTitle = "NO",
+                CancelButtonDelegate = () =>
+                {
+                    Debug.Log("NO");
+                },
+                okButtonTitle = "Yes",
+                OkButtonDelegate = () =>
+                {
+                    Debug.Log("Yes");
+                }
+            });
     }
 
     public void on_MainMenu_button_clicked()
@@ -54,19 +55,20 @@ public class PauseGame : MonoBehaviour {
         string title = "메인 메뉴 돌아가기";
         string text = "정말 메뉴로 돌아가시겠습니까?";
 
-        MessageBox.Show(title, text, new MsgOptions
-        {
-                    cancelButtonTitle = "NO",
-            CancelButtonDelegate = () =>
-                    {
-                        Debug.Log("NO");
-                    },
-                    okButtonTitle = "Yes",
-            OkButtonDelegate = () =>
-                    {
-                        EditorSceneManager.LoadScene("GameTitle");
-                    }
-                });
+        MessageBox.Show(title, text, 
+            new MsgOptions
+            {
+                cancelButtonTitle = "NO",
+                CancelButtonDelegate = () =>
+                {
+                    Debug.Log("NO");
+                },
+                okButtonTitle = "Yes",
+                OkButtonDelegate = () =>
+                {
+                    EditorSceneManager.LoadScene("GameTitle");
+                }
+            });
     }
 
     public void on_rejected_clicked()
---
+++ Assets/Script/UI/SkillUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillUI : MonoBehaviour {
+
+
+    public Texture2D tex;
+    public Image img;
+
+    
+
+	// Use this for initialization
+	void Start () {
+        
+        Rect rect = new Rect(0, 0, 82, tex.height);
+
+        img.sprite = Sprite.Create(tex, rect, new Vector2(0,0));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
