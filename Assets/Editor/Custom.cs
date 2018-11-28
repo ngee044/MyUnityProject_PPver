9c212df7e465cfd84abf473684817299f28f53a4
mgee044 <mgee044@naver.com>
2018-11-28 21:50:58 +0900

    commit

---
+++ Assets/Editor/Custom.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Custom : MonoBehaviour {
+
+    [MenuItem("Custom/Mode/TestCustom",false,1)]
+    static void Print()
+    {
+        Debug.Log("This is Custom Menu Clicked");
+    }
+}
---
+++ Assets/Editor/MarkTextEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(TextEditor))]
+public class MarkTextEditor : UnityEditor.UI.TextEditor
+{
+    GUIStyle GUIStype;
+
+    void Awake()
+    {
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        EditorGUILayout.LabelField("Extention", EditorStyles.boldLabel);
+    }
+
+}
