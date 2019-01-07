93b504d5b6b8621e3fd796cda4d3a99ad10ac3bd
mgee044 <mgee044@naver.com>
2019-01-07 12:37:39 +0900

    How Make to ObjectPool gernic template ??

---
+++ Assets/NewClass/RollABall/Space_Shooter/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool<T> : MonoBehaviour {
+
+    public GameObject Prefab { get; set; }
+    private List<T> Pool;
+
+    // Use this for initialization
+    void Start () {
+        Pool = new List<T>();
+    }
+
+#if false
+    public T GetFromPool()
+    {
+        for (int i = 0; i < Pool.Count; i++)
+        {
+            if (!Pool[i].gameObject.activeInHierarchy)
+            {
+                Pool[i].gameObject.SetActive(true);
+                return Pool[i];
+            }
+        }
+
+        T temp = Instantiate(Prefab);
+        Pool.Add(temp);
+        return temp;
+    }
+#endif
+}
