4d895e1268aef6788fa497bf84f1d42f30a49603
mgee044 <mgee044@naver.com>
2019-01-11 17:56:11 +0900

    Action Character Animation Add

---
+++ Assets/NewClass/RollABall/MyActionGame/Assets/Script/AnimationHashList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHashList : MonoBehaviour
+{
+    public static readonly int IsAttackHash = Animator.StringToHash("IsAttack");
+    public static readonly int IsDeadHash = Animator.StringToHash("IsDead");
+}
---
+++ Assets/NewClass/RollABall/MyActionGame/Assets/Script/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator Ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Ani.SetBool(AnimationHashList.IsAttackHash, true);
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Ani.SetBool(AnimationHashList.IsAttackHash, false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Ani.SetBool(AnimationHashList.IsDeadHash, true);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Ani.SetBool(AnimationHashList.IsDeadHash, false);
+        }
+    }
+}
