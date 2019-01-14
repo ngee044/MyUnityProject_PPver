be49791c9737a5bdd5cef49a68035b4d82459a94
mgee044 <mgee044@naver.com>
2019-01-14 17:48:02 +0900

    ActionGame Commit

--- Assets/NewClass/RollABall/MyActionGame/Assets/Script/AnimationHashList.cs
+++ Assets/NewClass/RollABall/MyActionGame/Assets/Script/AnimationHashList.cs
@@ -6,4 +6,5 @@ public class AnimationHashList : MonoBehaviour
 {
     public static readonly int IsAttackHash = Animator.StringToHash("IsAttack");
     public static readonly int IsDeadHash = Animator.StringToHash("IsDead");
+    public static readonly int IsWalkHash = Animator.StringToHash("IsMove");
 }
---
+++ Assets/NewClass/RollABall/MyActionGame/Assets/Script/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private Animator Ani;
+    private float speed;
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        Ani = GetComponent<Animator>();
+        speed = 2f;
+    }
+
+    void OnEnable()
+    {
+        rb2D.velocity = transform.right * speed;
+        Ani.SetBool(AnimationHashList.IsWalkHash, true);
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void FinishAttack()
+    {
+        Ani.SetBool(AnimationHashList.IsAttackHash, false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Find Player");
+            rb2D.velocity = Vector3.zero;
+            Ani.SetBool(AnimationHashList.IsWalkHash, false);
+            Ani.SetBool(AnimationHashList.IsAttackHash, true);
+        }
+    }
+}
