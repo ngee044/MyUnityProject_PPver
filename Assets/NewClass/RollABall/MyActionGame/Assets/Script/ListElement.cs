3486f2c6aed6a8e722155f514568232838544cc3
mgee044 <mgee044@naver.com>
2019-01-22 17:49:59 +0900

    list contents study commit

---
+++ Assets/NewClass/RollABall/MyActionGame/Assets/Script/ListElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListElement : MonoBehaviour
+{
+    
+    public Image icon;
+    public Text title, contents, purchase,cost;
+    public Button purchaseButton;
+    private int id;
+
+    public void Init
+        (Sprite inputIcon, string titleText, string costText, string contentsText, string purchaseText)
+    {
+        icon.sprite = inputIcon;
+        title.text = titleText;
+        cost.text = costText;
+        contents.text = contentsText;
+        purchase.text = purchaseText;
+        
+    }
+
+    public void Renew(string costText, string contentsText, string purchaseText)
+    {
+        cost.text = costText;
+        contents.text = contentsText;
+        purchase.text = purchaseText;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/NewClass/RollABall/MyActionGame/Assets/Script/PlayerStatusController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class stat
+{
+    public int iconId;
+    public string name;
+    public string contents; //fomat string
+    public int currentLv;
+
+    public float costBase;
+    public float costWeight;
+    public double costCurrent;
+
+    public float valueBase;
+    public float valueWeight;
+    public double valueCurrent;
+}
+
+public class PlayerStatusController : MonoBehaviour
+{
+    [SerializeField]
+    private stat[] infos;
+    [SerializeField]
+    private Sprite[] icons;
+    [SerializeField]
+    private ListElement[] elements;
+
+
+    private void Awake()
+    {
+        infos = new stat[2];
+
+        infos[0] = new stat();
+        infos[0].iconId = 0;
+        infos[0].name = "물약";
+        infos[0].contents = "HP <color=red>10</color> 회복";
+        infos[0].currentLv = 0;
+
+        infos[0].costBase = 100;
+        infos[0].costCurrent = 1.07f;
+        infos[0].costWeight = infos[0].costBase * Mathf.Pow(infos[0].costWeight, infos[0].currentLv);
+
+        infos[0].valueBase = 10;
+        infos[0].valueWeight = 1.04f;
+        infos[0].valueCurrent = infos[0].costBase * Mathf.Pow(infos[0].valueWeight, infos[0].currentLv);
+
+        for(int i = 0; i < elements.Length; i++)
+        {
+            elements[i].Init(icons[infos[i].iconId], 
+                             infos[i].name, 
+                             infos[i].costCurrent.ToString("f1"),
+                             string.Format(infos[i].contents, infos[i].valueCurrent.ToString("f1")),"구매");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
